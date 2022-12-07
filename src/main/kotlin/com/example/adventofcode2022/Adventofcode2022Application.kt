9dfe91983dd7e450088ff92cdcd747d780cc7b97
Gabor Matyi <gabor.matyi@gmail.com>
2022-12-07 17:47:04 +0100

    add spring boot kotlin starter project

---
+++ src/main/kotlin/com/example/adventofcode2022/Adventofcode2022Application.kt
@@ -0,0 +1,11 @@
+package com.example.adventofcode2022
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Adventofcode2022Application
+
+fun main(args: Array<String>) {
+	runApplication<Adventofcode2022Application>(*args)
+}
---
+++ src/test/kotlin/com/example/adventofcode2022/Adventofcode2022ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.adventofcode2022
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Adventofcode2022ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
